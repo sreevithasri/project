c2eca82d411e878842175706b0df4c62c64884ad
sreevithasri <152057780+sreevithasri@users.noreply.github.com>
2023-11-26 13:47:25 +0530

    Add files via upload

---
+++ task1.js
@@ -0,0 +1,73 @@
+const employees = [
+    {
+      id: "C101",
+      name: "kavya",
+      role: "Junior Web Developer",
+      experience: "2 Years",
+      payout: "25000",
+    },
+    {
+      id: "C102",
+      name: "janaki",
+      role: "Junior Web Developer",
+      experience: "1 Year",
+      payout: "20000",
+    },
+    {
+      id: "C103",
+      name: "sweatha",
+      role: "Front End Developer",
+      experience: "3 Years",
+      payout: "30000",
+    },
+    {
+      id: "C104",
+      name: "rakshana",
+      role: "Junior Web Developer",
+      experience: "3 Years",
+      payout: "25000",
+    },
+    {
+      id: "C105",
+      name: "baby nisha",
+      role: "Front End Developer",
+      experience: "4 Years",
+      payout: "35000",
+    },
+    {
+      id: "C106",
+      name: "velins",
+      role: "Back End Developer",
+      experience: "2 Years",
+      payout: "30000",
+    },
+    {
+      id: "C107",
+      name: "ramkumar",
+      role: "Senior Front End Developer",
+      experience: "5 Years",
+      payout: "45000",
+    },
+    {
+      id: "C108",
+      name: "jayanthi",
+      role: "BDE",
+      experience: "2 Years",
+      payout: "20000",
+    },
+    {
+      id: "C109",
+      name: "swathi",
+      role: "Business Associate",
+      experience: "1 Year",
+      payout: "15000",
+    },
+    {
+      id: "C110",
+      name: "David",
+      role: "Tech Lead",
+      experience: "4 Years",
+      payout: "65000",
+    },
+  ];
+  
